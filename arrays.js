1c81b9881db41a003210bd46b3023d68c49108a0
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-20 20:05:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
+array= [element, ...array];
+  return array;
 }
 function addElementToBeginningOfArray(array, element)
 {
