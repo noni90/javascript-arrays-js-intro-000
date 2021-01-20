49064965c5d7574728c138865c2a3c54288f8c7e
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-20 20:35:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array)
 }
  function removeElementFromBeginningOfArray(array)
  {
-   array.slice(1);
-   return array
+   return array.slice(1);
+   
  }
 function destructivelyRemoveElementFromEndOfArray(array)
 {
