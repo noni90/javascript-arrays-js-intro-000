0016f967080b552979ab39e23e428f90b27ca910
Noor Albayaty <noor.albayaty90@gmail.com>
2021-01-20 20:34:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function destructivelyAddElementToBeginningOfArray(array, element)
  {
    return [...array,element];
  }
- function addElementToEndOfArray(array, element)
+ function destructivelyAddElementToEndOfArray(array, element)
  {
    array.push(element);
    return array;
